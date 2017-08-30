dec4268ef9d8b67291a389eaf1c1f46ae1234950
Elias Araya <earayad@hotmail.cl>
2017-08-30 10:41:22 -0400

    Se añadió la comprobación de la existencia de una página

--- iniciarServidor.sh
+++ iniciarServidor.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+
 python servidor_demo.py
--- servidor_demo.py
+++ servidor_demo.py
@@ -4,18 +4,17 @@ import json
 from nose.tools import *
 
 class Server:
-	"""docstring for ClassName"""
+
 	def __init__(self, port = 9100):
 		self.host = '127.0.0.1'
 		self.port = port
-		self.www_dir = 'www'
+
 	def activate_server(self):
 		self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.socket.bind((self.host, self.port))
-		self.socket.listen(1)
-		self._wait_for_connections()
+		self._connection()
 
-	def _gen_headers(self, code):
+	def _headers(self, code):
 		h = ' '
 		if (code == 200):
 			h = 'HTTP/1.1 200 OK'
@@ -23,12 +22,14 @@ class Server:
 			h = 'HTTP/1.1 404 Not Found'
 		return h
 
-	def _wait_for_connections(self):
+	def _connection(self):
 		while True:
+			self.socket.listen(1)
 			conn, addr = self.socket.accept()
 			data = conn.recv(1024)
 			string = bytes.decode(data)
 			request_method = string.split(' ')[0]
+			print("\n*******************************************************************************")
 			print("Method:", request_method)
 			print("Request body: ", string)
 			if (request_method == 'GET') | (request_method == 'HEAD'):
@@ -37,22 +38,35 @@ class Server:
 				file_requested = file_requested.split('?')[0]
 				if (file_requested == '/'):
 					file_requested = '/index.html'
-				file_requested = 'c:/Sites/Taller/documentRoot'+file_requested
+				file_requested = "c:/Sites/Taller/documentRoot" + file_requested
+				print("Pagina solicitada [",file_requested,"]")
 				try:
 					file_handler = open(file_requested,'rb')
 					if (request_method == 'GET'):
 						response_content = file_handler.read()
 						print(response_content)
+						print("\n")
 					file_handler.close()
+					response_headers = self._headers(200)
+					print("HTTP/1.1 200 OK")
+					print("Aqui va Json Araos")
+					print("*******************************************************************************")
 				except Exception, e:
-					raise
-				else:
-					pass
-				finally:
-					pass
-			response_headers = self._gen_headers(200)
-			conn.sendall(response_headers)
-			conn.close()
+					print("\nHTTP/1.1 404 Not Found")
+					print("Json Araos")
+					response_headers = self._headers(404)
+					if (request_method == 'GET'):
+						response_content = b"<html><body><p>Error 404: File not foud</p></body></html>"
+				server_response = response_headers.encode()
+				if(request_method == 'GET'):
+					server_response += response_content
+				
+				conn.send(server_response)
+				conn.close()
+			else:
+				print("Unknown HTTP request method:", request_method)
+			
+			
 
 
 
